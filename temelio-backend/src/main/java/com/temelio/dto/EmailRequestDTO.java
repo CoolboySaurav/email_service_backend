79faf2bc60252dd0aaeb562844047e3dd0ec6d1a
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2025-02-04 13:47:28 -0700

    Update EmailRequestDTO.java
    Added cc, bcc fields

--- temelio-backend/src/main/java/com/temelio/dto/EmailRequestDTO.java
+++ temelio-backend/src/main/java/com/temelio/dto/EmailRequestDTO.java
@@ -2,9 +2,14 @@ package com.temelio.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class EmailRequestDTO {
-    private  String email;
     private String subject;
     private String contentTemplate;
+    private List<String> cc;
+    private List<String> bcc;
+
+    // Getters and setters
 }
