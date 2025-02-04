0800aafa5ba2b568eda3b9078310f8e04ee4c8ac
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2025-02-04 13:46:35 -0700

    Update EmailLog.java
    Added senderEmail, cc, bcc field

--- temelio-backend/src/main/java/com/temelio/model/EmailLog.java
+++ temelio-backend/src/main/java/com/temelio/model/EmailLog.java
@@ -1,25 +1,32 @@
 package com.temelio.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.*;
 import lombok.Data;
 
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
-@Table(name = "email_Logs")
-@Data
+@Table(name = "email_logs")
+
+@Data // Generates getters, setters, toString, equals, and hashCode
 public class EmailLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
+
+    private String senderEmail;
+
     private String recipientEmail;
 
     private String subject;
 
     private String content;
+
+    private List<String> cc;
+
+    private List<String> bcc;
 
     private LocalDateTime sentAt;
 }
